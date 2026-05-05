ad75769599a5dd032283f45950d76f06e31b3553
Daigo Fujiwara-Smith <daigo@daigofujiwara.com>
2026-05-05 09:06:34 -0400

    Fix 404 router bug, add analytics hook

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,26 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Route, Switch } from 'wouter'
+import { Route, Switch, useLocation } from 'wouter'
 
 import Home from './home/index'
 import Lab001 from './lab/001-sagarifuji/index'
 import Lab002 from './lab/002-rainbow-swarm/index'
 import Lab003 from './lab/003-stage/index'
 
+declare const gtag: (...args: unknown[]) => void
+
+function Analytics() {
+  const [location] = useLocation()
+  useEffect(() => {
+    if (typeof gtag === 'undefined') return
+    gtag('event', 'page_view', { page_path: location })
+  }, [location])
+  return null
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics />
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/lab/001-sagarifuji" component={Lab001} />
