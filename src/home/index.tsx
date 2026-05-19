1d563d174be0acbd88fc3e968c1ef3a94644728b
Daigo Fujiwara-Smith <daigo@daigofujiwara.com>
2026-05-18 20:36:41 -0400

    &apos;

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
       <header>
         <h1 className="home-name">Daigo Fujiwara-Smith</h1>
         <p className="home-tagline">
-          By day, I build <a href="">interactive journalism</a> at The Boston Globe's <a href="https://apps.bostonglobe.com/">Story Lab Interactive team</a>. Here I experiment with WebGPU, R3F, and and computational storytelling techniques and technologies I haven't figured out yet. Learning in public, one project at a time.
+          By day, I build <a href="">interactive journalism</a> at The Boston Globe&apos;s <a href="https://apps.bostonglobe.com/">Story Lab Interactive team</a>. Here I experiment with WebGPU, R3F, and and computational storytelling techniques and technologies I haven&apos;t figured out yet. Learning in public, one project at a time.
         </p>
       </header>
 
